44d5f895171da0602b12822a4197f8ce800b917b
Manas Gupta <manasg30361@gmail.com>
2026-03-03 15:02:14 +0530

    Configure Auth

--- config/db.js
+++ config/db.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose')
 
 const connectDB = async () => {
+    console.log("connectDB function called")   // 👈 ADD THIS
+
     try {
+        console.log("Trying MongoDB connection...")
         await mongoose.connect(process.env.MONGO_URI)
         console.log("MongoDB Connected")
     } catch (err) {
-        console.error("Database connection failed")
+        console.error("MongoDB Error:", err.message)
         process.exit(1)
     }
 }
