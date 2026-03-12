c9f90f9af3e99104af17cc748cb2336defb8a8af
Manas Gupta <manasg30361@gmail.com>
2026-03-12 23:27:36 +0530

    fetch and display user tasks in dashboard using authenticated API request

--- taskflow-frontend/src/pages/Dashboard.jsx
+++ taskflow-frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,32 @@
+import { useEffect, useState } from "react"
+import API from "../services/api"
+
 function Dashboard() {
+
+  const [tasks, setTasks] = useState([])
+
+  const fetchTasks = async () => {
+    try {
+
+      const token = localStorage.getItem("token")
+
+      const res = await API.get("/tasks", {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+
+      setTasks(res.data)
+
+    } catch (error) {
+      console.error("Error fetching tasks", error)
+    }
+  }
+
+  useEffect(() => {
+    fetchTasks()
+  }, [])
+
   return (
     <div className="min-h-screen bg-gray-100 p-10">
 
@@ -8,15 +36,18 @@ function Dashboard() {
 
       <div className="bg-white p-6 rounded-xl shadow-md">
 
-        <input
-          type="text"
-          placeholder="Add new task..."
-          className="border p-3 w-full rounded-lg mb-4"
-        />
-
-        <button className="bg-blue-600 text-white px-4 py-2 rounded-lg">
-          Add Task
-        </button>
+        {tasks.length === 0 ? (
+          <p>No tasks yet</p>
+        ) : (
+          tasks.map((task) => (
+            <div
+              key={task._id}
+              className="border p-3 mb-2 rounded-lg flex justify-between"
+            >
+              <span>{task.title}</span>
+            </div>
+          ))
+        )}
 
       </div>
 
